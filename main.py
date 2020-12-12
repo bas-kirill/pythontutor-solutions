23414e638fe7c9ed47845f30d522a0d95411714e
bas-kirill <people.can.you.can@gmail.com>
2020-12-12 17:05:10 +0300

    Sums: 4, 5

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-a = int(input())
-b = int(input())
-
-for i in range(a, b - 1, -1):
-    if i % 2 == 1:
-        print(i, end=' ')
+n = int(input())
+sum = 0
+for i in range(1, n + 1):
+    sum += i ** 3
+print(sum)
